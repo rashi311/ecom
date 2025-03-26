5f7b3a769074a59f0054b0a9833ef151f3e56959
rashi311 <rashi312002@gmail.com>
2025-03-26 20:53:58 +0530

    message

---
+++ ecomsite/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./templates/**/*.html',
+  './products/templates/**/*.html',
+  './cart/templates/**/*.html',
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
